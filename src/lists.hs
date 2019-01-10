400e48c8ce3bf82c8b880c59a9b276ced45c92fd
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-01-11 00:27:25 +0100

    ok more samples

--- src/lists.hs
+++ src/lists.hs
@@ -26,3 +26,26 @@ Prelude> 42:numbers
 [42,3,5,2,-4]
 Prelude> 
 
+GHCi, version 7.10.3: http://www.haskell.org/ghc/  :? for help
+Prelude> ["hello","world"] ++ ["to","all"]
+["hello","world","to","all"]
+Prelude> ['h','a','s'] ++ ['k','e','l','l']
+"haskell"
+Prelude> 3:[14, 5, 78]
+[3,14,5,78]
+Prelude> let list = [[2,3,55],[7,8,3],[5,1],[3,5]]  
+Prelude> list
+[[2,3,55],[7,8,3],[5,1],[3,5]]
+Prelude> list ++ [[9,0]]
+[[2,3,55],[7,8,3],[5,1],[3,5],[9,0]]
+Prelude> list ++ [9,0]
+
+<interactive>:9:1:
+    Non type-variable argument in the constraint: Num [t]
+    (Use FlexibleContexts to permit this)
+    When checking that ‘it’ has the inferred type
+      it :: forall t. (Num t, Num [t]) => [[t]]
+Prelude> list ++ 8
+
+<interactive>:10:1:
+
