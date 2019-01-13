d4e6730348bc31d0b76174628918c179aceb1343
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-01-13 23:06:08 +0100

    Ok, with cabal

---
+++ src/ranges.hs
@@ -0,0 +1,17 @@
+Prelude>
+Prelude> [1..4]
+[1,2,3,4]
+Prelude> [-2..5]
+[-2,-1,0,1,2,3,4,5]
+Prelude> ['a'..'z']
+"abcdefghijklmnopqrstuvwxyz"
+Prelude> ['D'..'J']
+"DEFGHIJ"
+Prelude> [1, 3..5]
+[1,3,5]
+Prelude> [1, 2..10]
+[1,2,3,4,5,6,7,8,9,10]
+Prelude> [1, 3..10]
+[1,3,5,7,9]
+Prelude>
+
