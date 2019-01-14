8ee934afee2bfb2dd179345e636aab93b5cd20cf
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-01-14 23:18:00 +0100

    Added ranges samples

--- src/ranges.hs
+++ src/ranges.hs
@@ -14,4 +14,21 @@ Prelude> [1, 2..10]
 Prelude> [1, 3..10]
 [1,3,5,7,9]
 Prelude>
+Prelude> take 4 (cycle "yes")
+"yesy"
+Prelude> take 9 (cycle "yes")
+"yesyesyes"
+Prelude> take 11 (cycle "yes")
+"yesyesyesye"
+Prelude> take 11 (cycle [0, 2, 2])
+[0,2,2,0,2,2,0,2,2,0,2]
+Prelude> take 9 (cycle 3)
+
+<interactive>:9:1:
+Non type-variable argument in the constraint: Num [a]
+(Use FlexibleContexts to permit this)
+When checking that ‘it’ has the inferred type
+it :: forall a. Num [a] => [a]
+Prelude> take 9 (repeat 3)
+[3,3,3,3,3,3,3,3,3]
 
