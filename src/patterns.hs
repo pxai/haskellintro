ae38281464a0b875db1ff5a35099514abceba248
Pello Altadill <pello_altadill@cuatrovientos.org>
2019-01-25 23:13:27 +0100

    Ok, it's something that works

--- src/patterns.hs
+++ src/patterns.hs
@@ -1,3 +1,9 @@
-hello :: (String a) => a -> String
+-- hello :: (String a) => a -> String
 hello "Pello" = "It's me"
 hello x = "Sorry, I don't know you"
+
+lucky 1 = "Nope"
+lucky 2 = "Neither"
+lucky 3 = "CORRECT"
+lucky 4 = "No Way"
+lucky x = "Not even close"
