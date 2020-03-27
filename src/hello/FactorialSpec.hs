8cefec8643c173bf89671cd8d2ece770d611578e
Pello Altadill <pello.altadill@gluckgames.com>
2020-03-27 09:28:21 +0100

    Little change

--- src/hello/FactorialSpec.hs
+++ src/hello/FactorialSpec.hs
@@ -9,4 +9,8 @@ main :: IO ()
 main = hspec $ do
   describe "Prelude.head" $ do
       it "returns 0 with 0" $ do
-            factorial 0 `shouldBe` (0 :: Integral a)
+            factorial 0 `shouldBe` (1 :: Int)
+      it "returns 2 with 2" $ do
+            factorial 2 `shouldBe` (2 :: Int)
+      it "returns 6 with 3" $ do
+            factorial 3 `shouldBe` (6 :: Int)
